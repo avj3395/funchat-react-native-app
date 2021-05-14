06d88659a34b9300e4075ead262a486b3f18c08a
Akshay <avj3395@gmail.com>
2021-05-14 14:53:10 +0530

    install some dependencies

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import api from "./api/api";
+import("./ReactotronConfig").then(() => console.log("Reactotron Configured"));
 
 export default function App() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     api
+  //       .get("/api/user/")
+  //       .then((res) => {
+  //         console.log("res===", res);
+  //       })
+  //       .catch((error) => {
+  //         console.log("err==", error);
+  //       });
+  //   };
+  //   fetchData();
+  // }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
